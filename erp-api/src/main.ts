9bb5dd778f820abaff8fbe07eba967230ba5480c
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-15 18:33:44 +0100

    set cookie-parser, helmet and cors

--- erp-api/src/main.ts
+++ erp-api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(helmet());
+  app.use(cookieParser());
   await app.listen(3002);
 }
 bootstrap();
