4d5ff02d81149b7689fe7f6c7ea8a4e10d60a03f
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-17 16:45:55 +0100

    add validation pipe

--- erp-api/src/main.ts
+++ erp-api/src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.use(helmet());
   app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3002);
 }
 bootstrap();
