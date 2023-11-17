3c1589ca8a39d81bb4523d8d808085ebf3d98e25
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-17 16:47:09 +0100

    refactor

--- erp-api/src/auth/auth.controller.ts
+++ erp-api/src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get, Post, UseGuards, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
-import { SaveUserEntity } from 'src/common/types';
+import { SaveUserEntity } from 'src/types';
 import { LocalAuthGuard } from 'src/common/guards/local-auth.guard';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
 
@@ -23,8 +23,7 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   @Post('/refresh-token')
   async refreshToken(@Req() req: Request) {
-    const oldToken = req.headers.authorization.split(' ')[1];
-    return this.authService.refreshToken(oldToken);
+    return this.authService.refreshToken(req);
   }
 
   @UseGuards(JwtAuthGuard)
