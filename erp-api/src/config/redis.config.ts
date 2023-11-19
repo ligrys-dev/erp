80a3efb87550232298bc0ad80e2e1d722fab3250
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-19 13:34:31 +0100

    fix nuber properties in config files

--- erp-api/src/config/redis.config.ts
+++ erp-api/src/config/redis.config.ts
@@ -4,7 +4,9 @@ import { redisStore } from 'cache-manager-redis-yet';
 
 export const getRedisConfig = async (configService: ConfigService) =>
   ({
-    store: await redisStore({ ttl: 60 * 60 * 1000 }),
+    store: await redisStore({
+      ttl: Number(configService.get('REDIS_TTL')) || 5 * 1000,
+    }),
     host: configService.get('REDIS_HOST') || 'localhost',
-    port: configService.get('REDIS_PORT') || 6379,
+    port: Number(configService.get('REDIS_PORT')) || 6379,
   }) as CacheModuleAsyncOptions;
