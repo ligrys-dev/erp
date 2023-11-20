5b3354145cdd1f979d08486f10e3c0576379cf1d
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-20 16:55:05 +0100

    implement create firm

--- erp-api/src/modules/firm/firm.service.ts
+++ erp-api/src/modules/firm/firm.service.ts
@@ -3,12 +3,16 @@ import { CreateFirmDto } from './dto/create-firm.dto';
 import { UpdateFirmDto } from './dto/update-firm.dto';
 import { Firm } from './entities/firm.entity';
 import { assignProperties } from 'src/utils/assign-properties';
+import { Address } from 'src/common/entities/address.entity';
 
 @Injectable()
 export class FirmService {
   async create(createFirmDto: CreateFirmDto) {
+    const { addressId, ...firmData } = createFirmDto;
     const firm = new Firm();
-    assignProperties(firm, createFirmDto);
+    assignProperties(firm, firmData);
+    // TODO implement adding new address
+    firm.address = await Address.findOneByOrFail({ id: addressId });
     return await firm.save();
   }
 
@@ -17,7 +21,7 @@ export class FirmService {
   }
 
   async findOne(id: string) {
-    return await Firm.findOneByOrFail({ id });
+    return await Firm.findOneOrFail({ where: { id }, relations: ['address'] });
   }
 
   async update(id: string, updateFirmDto: UpdateFirmDto) {
