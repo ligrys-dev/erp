9fd654a04cd9d4e9cafd6c08b90493209be4b13c
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-20 13:57:54 +0100

    add console.log in app.listen

--- erp-api/src/main.ts
+++ erp-api/src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
   app.use(helmet());
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3002);
+  await app.listen(3002, () => {
+    console.log(`Listening on port 3002`);
+  });
 }
 bootstrap();
