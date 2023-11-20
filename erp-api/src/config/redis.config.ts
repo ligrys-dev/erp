c8d9853186b20b589c5fb9e3236fe01b2ddbdfe3
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-20 13:58:07 +0100

    refactorimg

--- erp-api/src/config/redis.config.ts
+++ erp-api/src/config/redis.config.ts
@@ -5,8 +5,8 @@ import { redisStore } from 'cache-manager-redis-yet';
 export const getRedisConfig = async (configService: ConfigService) =>
   ({
     store: await redisStore({
-      ttl: Number(configService.get('REDIS_TTL')) || 5 * 1000,
+      ttl: +configService.get('REDIS_TTL') || 5 * 1000,
     }),
     host: configService.get('REDIS_HOST') || 'localhost',
-    port: Number(configService.get('REDIS_PORT')) || 6379,
+    port: +configService.get('REDIS_PORT') || 6379,
   }) as CacheModuleAsyncOptions;
--- erp-api/src/config/typeorm.config.ts
+++ erp-api/src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ export const getTypeOrmConfig = (configService: ConfigService) =>
   ({
     type: 'postgres',
     host: configService.get('DB_HOST') || 'localhost',
-    port: Number(configService.get('DB_PORT')) || 5432,
+    port: +configService.get('DB_PORT') || 5432,
     username: configService.get('DB_USERNAME'),
     password: configService.get('DB_PASSWORD'),
     database: configService.get('DB_DATABASE'),
