3880d74be72606579ae412b579d0289680d80e18
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-20 17:35:14 +0100

    implement create and find client methods

--- erp-api/src/modules/client/client.service.ts
+++ erp-api/src/modules/client/client.service.ts
@@ -3,13 +3,17 @@ import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { Client } from './entities/client.entity';
 import { assignProperties } from 'src/utils/assign-properties';
+import { Address } from 'src/common/entities/address.entity';
 
 @Injectable()
 export class ClientService {
   async create(createClientDto: CreateClientDto) {
+    const { addressId, ...clientData } = createClientDto;
     const client = new Client();
-    assignProperties(client, createClientDto);
-    return await client.save();
+    assignProperties(client, clientData);
+    client.address = await Address.findOneByOrFail({ id: addressId });
+    const { id } = await client.save();
+    return { id };
   }
 
   async findAll() {
@@ -17,7 +21,10 @@ export class ClientService {
   }
 
   async findOne(id: string) {
-    return await Client.findOneByOrFail({ id });
+    return await Client.findOneOrFail({
+      where: { id },
+      relations: ['address'],
+    });
   }
 
   async update(id: string, updateClientDto: UpdateClientDto) {
